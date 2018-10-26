2490c60fa3ef3f95cfec25cd3917a2373e151a86
Simon Haegler <shaegler@esri.com>
2018-10-26 20:41:34 +0200

    #91: add h17 to conan

--- src/conanfile.py
+++ src/conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile
 
 class PalladioConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "houdini/[>16.5.0,<17.0.0]@sidefx/stable",\
+    requires = "houdini/[>16.5.0,<18.0.0]@sidefx/stable",\
                "boost/1.66.0@conan/stable", \
                "catch2/2.0.1@bincrafters/stable"
     generators = "cmake"
